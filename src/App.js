82c80df43c79d0e9929c785e18a4459a52ec069d
Nathan Wang <nathan123wang@gmail.com>
2022-12-22 14:02:50 +1000

    Bug Fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import Header from './components/Header'
-function App() {
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th a5 2:30pm',
+        reminder: 'true',
+    }
+  ])
+
+  // Create a function to delete tasks
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
   return (
     <div className='container'>
       <Header />
+      <Tasks tasks = {tasks} onDelete = {deleteTask}/>
     </div>
   );
 }
---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types'
+
+const Button = ({color, text}) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
+    return (
+        <button 
+        onClick = {onClick} 
+        style = {{backgroundColor: color}} 
+        className = 'btn'
+        >
+            {text}
+        </button>
+  )
+}
+
+Button.defaultProps = {
+    color: 'steeleblue'
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+export default Button
