31ace14f38cfbb7add786f6d3bb9eb0598f47a13
Nathan Wang <nathan123wang@gmail.com>
2022-12-23 15:46:07 +1000

    Button Toggle Complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 import { useState } from 'react'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState (true)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -17,12 +19,36 @@ const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle reminder
+const toggleReminder = (id) => {
+  setTasks(
+    tasks.map((task) => 
+      task.id === id ? { ...task, reminder: 
+      !task.reminder } : task
+    ) 
+  )
+}
+
+const addTask= (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+  const newTask = {id, ...task}
+  setTasks([...tasks, newTask])
+}
+
   return (
     <div className='container'>
-      <Header />
-      <Tasks tasks = {tasks} onDelete = {deleteTask}/>
+      <Header onAdd = {() => setShowAddTask
+        (!showAddTask)} showAddTask = {showAddTask}/>
+      {showAddTask && <AddTask onAdd = {addTask}/>}
+      {tasks.length > 0 ? ( 
+        <Tasks tasks = {tasks} 
+        onDelete = {deleteTask}  
+        onToggle={toggleReminder} />
+      ) : (
+        'No tasks to Show'
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
